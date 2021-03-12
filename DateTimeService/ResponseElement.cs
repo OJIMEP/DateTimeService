4baf7f56c4ccd2ea41436c29607a74c5cbd21c01
Андрей Бородавко <a.borodavko@21vek.by>
2021-03-12 18:10:36 +0300

    база сделана

---
+++ DateTimeService/ResponseElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateTimeService
+{
+    public class ResponseElement
+    {
+        public string Nomenclature_id { get; set; }
+
+        public int Max_available_count { get; set; }
+
+    }
+}
