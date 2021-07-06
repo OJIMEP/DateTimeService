883bccca1801766647f7daf566d7f5ba4719a3a9
Андрей Бородавко <a.borodavko@21vek.by>
2021-07-06 17:21:59 +0300

    всё что в приложении кроме запроса

--- DateTimeService/Controllers/DateTimeController.cs
+++ DateTimeService/Controllers/DateTimeController.cs
@@ -810,6 +810,367 @@ namespace DateTimeService.Controllers
         }
 
 
+        [Authorize(Roles = UserRoles.AvailableDate + "," + UserRoles.Admin)]
+        [Route("AvailableDateWithCount")]
+        [HttpPost]
+        public async Task<IActionResult> AvailableDateWithCountAsync([FromBody] RequestDataAvailableDateByCodeItemsDTO inputDataJson)
+        {
+
+            OkObjectResult result;
+
+            var data = _mapper.Map<RequestDataAvailableDate>(inputDataJson);
+            string databaseType = "";
+            Stopwatch stopwatchExecution = new();
+            stopwatchExecution.Start();
+
+            var logElementLoadBal = new ElasticLogElement
+            {
+                Path = HttpContext.Request.Path,
+                Host = HttpContext.Request.Host.ToString(),
+                RequestContent = JsonSerializer.Serialize(data),
+                Id = Guid.NewGuid().ToString(),
+                AuthenticatedUser = User.Identity.Name
+            };
+
+
+            //string connString;
+            SqlConnection conn;
+            Stopwatch watch = new();
+            watch.Start();
+            try
+            {
+                //connString = await _loadBalacing.GetDatabaseConnectionAsync();
+                var dbConnection = await _loadBalacing.GetDatabaseConnectionAsync();
+                conn = dbConnection.connection;
+                databaseType = dbConnection.databaseType;
+
+            }
+            catch (Exception ex)
+            {
+                //connString = "";
+                logElementLoadBal.TimeSQLExecution = 0;
+                logElementLoadBal.ErrorDescription = ex.Message;
+                logElementLoadBal.Status = "Error";
+                var logstringElement1 = JsonSerializer.Serialize(logElementLoadBal);
+                _logger.LogInformation(logstringElement1);
+                return StatusCode(503);
+            }
+            watch.Stop();
+
+
+            if (conn == null)
+            {
+                logElementLoadBal.TimeSQLExecution = 0;
+                logElementLoadBal.ErrorDescription = "Не найдено доступное соединение к БД";
+                logElementLoadBal.Status = "Error";
+                logElementLoadBal.LoadBalancingExecution = watch.ElapsedMilliseconds;
+                var logstringElement1 = JsonSerializer.Serialize(logElementLoadBal);
+                _logger.LogInformation(logstringElement1);
+
+                Dictionary<string, string> errorDesc = new();
+                errorDesc.Add("ErrorDescription", "Не найдено доступное соединение к БД");
+
+                return StatusCode(500, errorDesc);
+            }
+
+            ResponseAvailableDate dbResult = new();
+
+            var logElement = new ElasticLogElement
+            {
+                Path = HttpContext.Request.Path,
+                Host = HttpContext.Request.Host.ToString(),
+                RequestContent = JsonSerializer.Serialize(data),
+                Id = Guid.NewGuid().ToString(),
+                DatabaseConnection = LoadBalancing.RemoveCredentialsFromConnectionString(conn.ConnectionString),
+                AuthenticatedUser = User.Identity.Name,
+                LoadBalancingExecution = watch.ElapsedMilliseconds
+            };
+
+            logElement.AdditionalData.Add("Referer", Request.Headers["Referer"].ToString());
+            logElement.AdditionalData.Add("User-Agent", Request.Headers["User-Agent"].ToString());
+            logElement.AdditionalData.Add("RemoteIpAddress", Request.HttpContext.Connection.RemoteIpAddress.ToString());
+
+
+            watch.Reset();
+
+            var Parameters1C = new List<GlobalParam1C>
+            {
+                new GlobalParam1C
+                {
+                    Name = "rsp_КоличествоДнейЗаполненияГрафика",
+                    DefaultDouble = 5
+                },
+                new GlobalParam1C
+                {
+                    Name = "КоличествоДнейАнализаЛучшейЦеныПриОтсрочкеЗаказа",
+                    DefaultDouble = 4
+                },
+                new GlobalParam1C
+                {
+                    Name = "ПроцентДнейАнализаЛучшейЦеныПриОтсрочкеЗаказа",
+                    DefaultDouble = 3
+                }
+            };
+            watch.Start();
+            GlobalParam1C.FillValues(conn, Parameters1C, _logger);
+            watch.Stop();
+            logElement.GlobalParametersExecution = watch.ElapsedMilliseconds;
+            watch.Reset();
+
+            long sqlCommandExecutionTime = 0;
+            watch.Start();
+            try
+            {
+                if (data.Codes.Length == 0 || (data.Codes.Length == 1 && data.Codes[0] == null))
+                {
+                    throw new Exception("Пустой массив кодов");
+                }
+
+                //sql connection object
+                //using SqlConnection conn = new(connString);
+
+                conn.StatisticsEnabled = true;
+
+                //open connection
+                //conn.Open();
+
+                string query = Queries.AvailableDateWithCount;
+
+                var DateMove = DateTime.Now.AddMonths(24000);
+                var TimeNow = new DateTime(2001, 1, 1, DateMove.Hour, DateMove.Minute, DateMove.Second);
+                var EmptyDate = new DateTime(2001, 1, 1, 0, 0, 0);
+                var MaxDate = new DateTime(5999, 11, 11, 0, 0, 0);
+
+                SqlCommand cmd = new(query, conn);
+
+
+                List<string> pickups = new();
+
+                var queryTextBegin = TextFillGoodsTable(data, cmd, true, pickups);
+
+                if (_configuration.GetValue<bool>("disableKeepFixedPlan"))
+                {
+                    query = query.Replace(", KEEPFIXED PLAN", "");
+                    queryTextBegin = queryTextBegin.Replace(", KEEPFIXED PLAN", "");
+                }
+
+                //define the SqlCommand object
+                List<string> pickupParameters = new();
+                foreach (var pickupPoint in pickups)
+                {
+                    var parameterString = string.Format("@PickupPointAll{0}", pickups.IndexOf(pickupPoint));
+                    pickupParameters.Add(parameterString);
+                    cmd.Parameters.Add(parameterString, SqlDbType.NVarChar, 4);
+                    cmd.Parameters[parameterString].Value = pickupPoint;
+                }
+                if (pickupParameters.Count == 0)
+                {
+                    pickupParameters.Add("NULL");
+                }
+
+
+                cmd.Parameters.Add("@P_CityCode", SqlDbType.NVarChar, 10);
+                cmd.Parameters["@P_CityCode"].Value = data.CityId;
+
+                cmd.Parameters.Add("@P_DateTimeNow", SqlDbType.DateTime);
+                cmd.Parameters["@P_DateTimeNow"].Value = DateMove;
+
+                cmd.Parameters.Add("@P_DateTimePeriodBegin", SqlDbType.DateTime);
+                cmd.Parameters["@P_DateTimePeriodBegin"].Value = DateMove.Date;
+
+                cmd.Parameters.Add("@P_DateTimePeriodEnd", SqlDbType.DateTime);
+                cmd.Parameters["@P_DateTimePeriodEnd"].Value = DateMove.Date.AddDays(Parameters1C.First(x => x.Name.Contains("rsp_КоличествоДнейЗаполненияГрафика")).ValueDouble - 1);
+
+                cmd.Parameters.Add("@P_TimeNow", SqlDbType.DateTime);
+                cmd.Parameters["@P_TimeNow"].Value = TimeNow;
+
+                cmd.Parameters.Add("@P_EmptyDate", SqlDbType.DateTime);
+                cmd.Parameters["@P_EmptyDate"].Value = EmptyDate;
+
+                cmd.Parameters.Add("@P_MaxDate", SqlDbType.DateTime);
+                cmd.Parameters["@P_MaxDate"].Value = MaxDate;
+
+                cmd.Parameters.Add("@P_DaysToShow", SqlDbType.Int);
+                cmd.Parameters["@P_DaysToShow"].Value = 7;
+
+                cmd.CommandTimeout = 5;
+
+
+                var dateTimeNowOptimizeString = "";
+                if (_configuration.GetValue<bool>("optimizeDateTimeNowEveryHour"))
+                {
+                    dateTimeNowOptimizeString = DateMove.ToString("yyyy-MM-ddTHH:00:00");
+                }
+                else
+                {
+                    dateTimeNowOptimizeString = dateTimeNowOptimizeString = DateMove.Date.ToString("yyyy-MM-ddTHH:mm:ss");
+                }
+
+                var pickupWorkingHoursJoinType = _configuration.GetValue<string>("pickupWorkingHoursJoinType");
+
+                string useIndexHint = _configuration.GetValue<string>("useIndexHintWarehouseDates");// @", INDEX([_InfoRg23830_Custom2])";
+                if (databaseType != "replica_tables")
+                {
+                    useIndexHint = "";
+                }
+
+                cmd.CommandText = queryTextBegin + string.Format(query, string.Join(",", pickupParameters),
+                    dateTimeNowOptimizeString,
+                    DateMove.Date.ToString("yyyy-MM-ddTHH:mm:ss"),
+                    DateMove.Date.AddDays(Parameters1C.First(x => x.Name.Contains("rsp_КоличествоДнейЗаполненияГрафика")).ValueDouble - 1).ToString("yyyy-MM-ddTHH:mm:ss"),
+                    Parameters1C.First(x => x.Name.Contains("КоличествоДнейАнализаЛучшейЦеныПриОтсрочкеЗаказа")).ValueDouble,
+                    Parameters1C.First(x => x.Name.Contains("ПроцентДнейАнализаЛучшейЦеныПриОтсрочкеЗаказа")).ValueDouble,
+                    pickupWorkingHoursJoinType, useIndexHint);
+
+
+                //execute the SQLCommand
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                //check if there are records
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        var article = dr.GetString(0);
+                        var code = dr.GetString(1);
+                        var availableDateCourier = dr.GetDateTime(2).AddMonths(-24000);
+                        var availableDateSelf = dr.GetDateTime(3).AddMonths(-24000);
+
+                        dbResult.Article.Add(article);
+                        dbResult.Code.Add(code);
+                        dbResult.Courier.Add(new(availableDateCourier));
+                        dbResult.Self.Add(new(availableDateSelf));
+                    }
+                }
+
+                var stats = conn.RetrieveStatistics();
+                sqlCommandExecutionTime = (long)stats["ExecutionTime"];
+
+                //close data reader
+                dr.Close();
+
+                //close connection
+                //conn.Close();
+
+                logElement.TimeSQLExecution = sqlCommandExecutionTime;
+                logElement.ResponseContent = JsonSerializer.Serialize(dbResult);
+                logElement.Status = "Ok";
+                logElement.AdditionalData.Add("stats", JsonSerializer.Serialize(stats));
+            }
+            catch (Exception ex)
+            {
+                logElement.TimeSQLExecution = sqlCommandExecutionTime;
+                logElement.ErrorDescription = ex.Message;
+                logElement.Status = "Error";
+            }
+
+            conn.Close();
+
+            var resultDict = new ResponseAvailableDateDict();
+
+            try
+            {
+                foreach (var codeItem in data.Codes)
+                {
+
+                    if (data.Codes.Length == 0 || (data.Codes.Length == 1 && data.Codes[0] == null))
+                    {
+                        break;
+                    }
+
+                    var resultElement = new ResponseAvailableDateDictElement
+                    {
+                        Code = codeItem.Article,
+                        SalesCode = codeItem.SalesCode,
+                        Courier = null,
+                        Self = null
+                    };
+
+
+                    int dbResultIndex = -1;
+                    if (String.IsNullOrEmpty(codeItem.Code))
+                    {
+                        dbResultIndex = dbResult.Article.FindIndex(s => s == codeItem.Article);
+                    }
+                    else
+                    {
+                        dbResultIndex = dbResult.Code.FindIndex(s => s == codeItem.Code);
+                    }
+
+                    if (dbResultIndex == -1)
+                        continue;
+
+                    resultElement.Courier = data.DeliveryTypes.Contains("courier") && dbResult.Courier[dbResultIndex].Year != 3999
+                        ? dbResult.Courier[dbResultIndex].Date.ToString("yyyy-MM-ddTHH:mm:ss")
+                        : null;
+                    resultElement.Self = data.DeliveryTypes.Contains("self") && dbResult.Self[dbResultIndex].Year != 3999
+                        ? dbResult.Self[dbResultIndex].Date.ToString("yyyy-MM-ddTHH:mm:ss")
+                        : null;
+
+                    if (String.IsNullOrEmpty(codeItem.Code))
+                    {
+                        resultDict.Data.Add(codeItem.Article, resultElement);
+                    }
+                    else
+                    {
+                        resultDict.Data.Add(String.Concat(codeItem.Article, "_", codeItem.SalesCode), resultElement);
+                    }
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                logElement.TimeSQLExecution = sqlCommandExecutionTime;
+                logElement.ErrorDescription = "Duplicated keys in dictionary";
+                logElement.Status = "Error";
+                logElement.AdditionalData.Add("Key", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                logElement.TimeSQLExecution = sqlCommandExecutionTime;
+                logElement.ErrorDescription = ex.Message;
+                logElement.Status = "Error";
+            }
+
+
+
+            if (data.DeliveryTypes.Contains("self") && data.DeliveryTypes.Contains("courier"))
+            {
+                var resultBoth = new ResponseAvailableDateDictBothDates();
+
+                foreach (var item in resultDict.Data)
+                {
+                    var newItem = new ResponseAvailableDateDictElementBothDates
+                    {
+                        Code = item.Value.Code,
+                        SalesCode = item.Value.SalesCode,
+                        Courier = item.Value.Courier,
+                        Self = item.Value.Self
+                    };
+
+                    resultBoth.Data.Add(item.Key, newItem);
+                }
+
+                result = Ok(resultBoth);
+            }
+            else
+            {
+                result = Ok(resultDict);
+            }
+
+            watch.Stop();
+            logElement.TimeSQLExecutionFact = watch.ElapsedMilliseconds;
+
+            stopwatchExecution.Stop();
+            logElement.TimeFullExecution = stopwatchExecution.ElapsedMilliseconds;
+
+            var logstringElement = JsonSerializer.Serialize(logElement);
+
+            _logger.LogInformation(logstringElement);
+
+            return result;
+        }
+
+
         private static string TextFillGoodsTable(RequestIntervalList data, SqlCommand cmdGoodsTable, bool optimizeRowsCount)
         {
             RequestDataAvailableDate convertedData = new()
