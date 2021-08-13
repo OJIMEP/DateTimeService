05b35e6ccfac61a1806d0520dba870a26b89e5af
Андрей Бородавко <a.borodavko@21vek.by>
2021-08-13 17:33:45 +0300

    добавлено расположение в логгер

--- DateTimeService/Logging/HttpLoggerProvider.cs
+++ DateTimeService/Logging/HttpLoggerProvider.cs
@@ -7,15 +7,17 @@ namespace DateTimeService
         private readonly string host;
         private readonly int port;
         private readonly int portHttp;
-        public HttpLoggerProvider(string _host, int _port, int _portHttp)
+        private readonly string env;
+        public HttpLoggerProvider(string _host, int _port, int _portHttp, string _env)
         {
             host = _host;
             port = _port;
             portHttp = _portHttp;
+            env = _env;
         }
         public ILogger CreateLogger(string categoryName)
         {
-            return new HttpLogger(host, port, portHttp);
+            return new HttpLogger(host, port, portHttp, env);
         }
 
         public void Dispose()
